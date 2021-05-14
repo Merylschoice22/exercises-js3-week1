5f6947c5274a5eb1de719cf04f6993dcb46583f4
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-15 00:17:03 +0200

    Good Practices finished

--- InClass/C-Good Design/exercise1.js
+++ InClass/C-Good Design/exercise1.js
@@ -9,31 +9,31 @@
 2. Commenting: the function isn't documented at all. It's very difficult to
    understand what the function's purpose is and how each part of the code
    contributes to it. By writing comments, the coder communicates their
-   reasoning and helps the function be human readable.
+   reasoning and helps the function be human readable. <====
 
 3. Layout/formatting: unnecessary spacing between the if and else statement. <====
 
 4./ Single responsibility: the function doesn't have a single purpose. It
    calculates national insurance and salary deductions. Maybe the national
-   insurance calculation could be moved to a separate function.
+   insurance calculation could be moved to a separate function. <==== 
 
 5. Input variable being overwritten: the function requires gross salary (before
    deductions) and net salary (after deductions) the `salary` input variable is
    therefore copied into an `originalSalary` variable so that it can be changed.
    It would be much clearer to create a new `netSalary` variable and leave
-   `salary` unmodified.
+   `salary` unmodified. <====
 
 6. DRY principle: the function validates the DRY (Don't Repeat Yourself) rule.
    The line where a deduction is taken from the salary is repeated 3 times with
-   different indices. This can be replaced with a `for` loop.
+   different indices. This can be replaced with a `for` loop. <====
 
 7. Magic numbers. The code contains a lot of magic numbers, including `17775`,
-   `0.09` and `0.1`.
+   `0.09` and `0.1`. 17775
 
 8./ Useless parameters: the code contains a variable which isn't used. They
    should be removed because they are confusing. It is tempting when you're
    starting to code a function to add more parameters thinking that you might
-   need them, but it's important to remove them if you don't end up using them.
+   need them, but it's important to remove them if you don't end up using them. <====
    
    */
 
@@ -69,48 +69,59 @@
 
 // console.log(calculateIncome(28000, "1150L", 1000, 580, false));
 
+///////////////////////////////////////////
 /***** NEW CODE *****/
 
+/** STARTING VARIABLES **/
+let nationalInsurance = 0;
+const tax1150L = 0.1;
+const taxST = 0.5;
+const taxDefault = 0.8;
+const incomeTaxes = [1000, 580];
+const studentLoanFixed = 17775;
+const studentLoanInterest = 0.09;
+
+/*** CALCULATE NATIONAL INSURANCE FUNCTION ***/
+
 function calculateNationalInsurance(taxCode, salary) {
-  let nationalInsurance = 0;
   if (taxCode === "1150L") {
-    nationalInsurance = salary * 0.1;
+    nationalInsurance = salary * tax1150L;
   } else if (taxCode === "ST") {
-    nationalInsurance = salary * 0.05;
+    nationalInsurance = salary * taxST;
   } else {
-    nationalInsurance = salary * 0.08;
+    nationalInsurance = salary * taxDefault;
   }
   return nationalInsurance;
 }
 
+/*** CALCULATE INCOME ***/
+
 function calculateIncome(salary, taxCode, incomeTaxes) {
-  console.log(incomeTaxes);
+  /** CALCULATE DEDUCTIONS **/
+  // NATIONAL INSURANCE
+  calculateNationalInsurance(taxCode, salary);
+
+  // TOTAL INCOME TAX
   var totalIncomeTax = 0;
   incomeTaxes.forEach((incomeTax) => {
     totalIncomeTax += incomeTax;
   });
-  console.log(totalIncomeTax);
 
-  var studentLoan = (salary - 17775) * 0.09;
-  var originalSalary = salary;
-  calculateNationalInsurance(taxCode, salary);
+  // STUDENT LOAN
+  var studentLoan = (salary - studentLoanFixed) * studentLoanInterest;
+
+  // FINAL SALARY
+  var netSalary = salary;
   var deductions = [nationalInsurance, totalIncomeTax, studentLoan];
-  deductions.forEach((deduction) => (salary -= deduction));
-
-  //   for (i=0; i > deductions.length - 1; i++) {
-  //      const deduction = deductions[i];
-  //      salary = salary - deduction;
-  //   }
+  deductions.forEach((deduction) => (netSalary -= deduction));
 
   return (
     "Your gross income is �" +
-    originalSalary.toString() +
-    " and your net income is �" +
     salary.toString() +
+    " and your net income is �" +
+    netSalary.toString() +
     "."
   );
 }
 
-const incomeTaxes = [1000, 580];
-calculateNationalInsurance("1150L", 28000);
 console.log(calculateIncome(28000, "1150L", incomeTaxes));
